480b0692b99525fd383b3218a0f32d476869a556
Tuna0128 <tn0128wayne@gmail.com>
2016-12-15 14:35:04 +0800

    Update ESS
    added Members & Athletes

---
+++ src/ESS/Athletes.java
@@ -0,0 +1,10 @@
+package ESS;
+
+public class Athletes extends Members{
+
+	public Athletes(String Name, int ID, int Age, String Country, String sportType) {
+		super(Name, ID, Age, Country, sportType);
+	}
+	
+	
+}
---
+++ src/ESS/Members.java
@@ -0,0 +1,64 @@
+package ESS;
+
+public class Members implements Comparable<Object> {
+	private String Name;
+	private int ID;
+	private int Age;
+	private String Country;
+	private String sportType;
+
+	public Members(String Name, int ID, int Age, String Country, String sportType) {
+		this.setName(Name);
+		this.setID(ID);
+		this.setAge(Age);
+		this.setCountry(Country);
+		this.setSportType(sportType);
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public int getAge() {
+		return Age;
+	}
+
+	public void setAge(int age) {
+		Age = age;
+	}
+
+	public String getCountry() {
+		return Country;
+	}
+
+	public void setCountry(String country) {
+		Country = country;
+	}
+
+	public String getSportType() {
+		return sportType;
+	}
+
+	public void setSportType(String sportType) {
+		this.sportType = sportType;
+	}
+
+	@Override
+	public int compareTo(Object arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
