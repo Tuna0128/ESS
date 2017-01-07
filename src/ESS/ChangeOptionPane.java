5b9a3b52ac818100c92da5ec4e1754be28d3c688
Tuna0128 <tn0128wayne@gmail.com>
2017-01-08 01:29:11 +0800

    Update ESS
    fixed some bugs
    changed font color
    adjusted layout

--- src/ESS/ChangeOptionPane.java
+++ src/ESS/ChangeOptionPane.java
@@ -3,7 +3,9 @@ package ESS;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JDialog;
@@ -15,51 +17,110 @@ import javax.swing.WindowConstants;
 public class ChangeOptionPane implements ActionListener {
 	JFrame f;
 	JList list;
-
+	DefaultListModel typeModel;
+	int value;
+	int mType = JOptionPane.QUESTION_MESSAGE;
 	/*
 	 * public static void main(String argv[]) { new ChangeOptionPane(); }
 	 */
 
-	public ChangeOptionPane(DefaultListModel timeModel, DefaultListModel typeModel, 
-			DefaultListModel teamModel, DefaultListModel venueModel, int value) {
-
+	public ChangeOptionPane(DefaultListModel timeModel, DefaultListModel typeModel, DefaultListModel teamModel,
+			DefaultListModel venueModel, int value, Venues[] venueList, int venueCounter) {
+		this.typeModel = typeModel;
+		this.value = value;
 		// Build Elements
-		int mType = JOptionPane.INFORMATION_MESSAGE;
+
 		/*
 		 * String in1 = JOptionPane.showInputDialog(f, "請輸入姓名", "輸入", mType);
 		 * JOptionPane.showMessageDialog(f, "您輸入的是 : " + in1);
 		 */
 		// String[] gender = { "男", "女" };
-		String[] team = new String[teamModel.getSize()];
-		for (int i = 0; i < teamModel.getSize(); i++) {
-			team[i] = (i + 1) + " " + (String) teamModel.getElementAt(i);
-		}
-		
-		while (true) {
-			String in2 = (String) JOptionPane.showInputDialog(f, "請選擇賽程", "輸入", mType, null, team, "未選擇");
-			System.out.println(in2);
-			if(in2==null)
-				break;
-			System.out.println(1);
-			int change = Integer.parseInt(in2.charAt(0) + "") - 1;
-			System.out.println("1:"+typeModel.getElementAt(value)+"2:"+typeModel.getElementAt(change));
-			if(typeModel.getElementAt(value).equals(typeModel.getElementAt(change))){
-				String buff = (String) teamModel.getElementAt(value);
-				teamModel.set(value, teamModel.getElementAt(change));
-				teamModel.set(change, buff);
-				break;
-			}
-			else{
-				JOptionPane.showMessageDialog(f,"更改失敗\n選取的賽事型態不符");
-			}
-		}
-		
-		//in2.charAt(0);
+
+		String[] selectType = { "未選取", "更換隊伍", "更換場地", "更換日期" };
+		String getSelect = (String) JOptionPane.showInputDialog(f, "<html><font color=white>請選擇更換項目</font><p></html>", 
+				"選取", mType, null, selectType, "未選取");
+		if (getSelect == "未選取" || getSelect == null)
+			return;
+		else if (getSelect == "更換隊伍")
+			changeOperation(teamModel);
+		else if (getSelect == "更換場地")
+			changeOperation(venueModel, timeModel, venueList, venueCounter);
+
+		// in2.charAt(0);
 		// Close JFrame
 		// f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	}
 
 	public void actionPerformed(ActionEvent e) {
 		// if (e.getSource()==id) {}
+	}
+
+	private void changeOperation(DefaultListModel inputModel) {
+		String[] team = new String[inputModel.getSize()];
+		for (int i = 0; i < inputModel.getSize(); i++) {
+			team[i] = (i + 1) + " " + inputModel.getElementAt(i);
+		}
+		while (true) {
+			String getNum = (String) JOptionPane.showInputDialog(f, "<html><font color=white>請選擇隊伍</font><p></html>", 
+					"選取", mType, null, team, "未選擇");
+			System.out.println(getNum);
+			if (getNum == null)
+				break;
+			System.out.println(getNum.split(" ")[0]);
+			int change = Integer.parseInt(getNum.split(" ")[0]) - 1;
+			System.out.println("1:" + typeModel.getElementAt(value) + " " + value + " " + "2:"
+					+ typeModel.getElementAt(change) + " " + change + " ");
+			// System.out.println(typeModel.toString());
+			if (typeModel.getElementAt(value).equals(typeModel.getElementAt(change))) {
+				String buff = (String) inputModel.getElementAt(value);
+				inputModel.set(value, inputModel.getElementAt(change));
+				inputModel.set(change, buff);
+				break;
+			} else {
+				JOptionPane.showMessageDialog(f, "<html><font color=white>更改失敗<br>選取隊伍的賽事型態不符</font><p></html>");
+			}
+		}
+	}
+
+	private void changeOperation(DefaultListModel inputModel, DefaultListModel timeModel,Venues[] venueList,int venueCounter) {
+		List<String> venueAL = new ArrayList<String>();
+		System.out.println(venueCounter);
+		for (int i = 0; i < venueCounter; i++) {
+			if (venueList[i].getSportType().equals(typeModel.getElementAt(value)))
+				venueAL.add(1+" "+venueList[i].getName());
+		}
+		
+		while (true) {
+			String getNum = (String) JOptionPane.showInputDialog(f, "<html><font color=white>請選擇場地</font><p></html>"
+					, "選取", mType, null,venueAL.toArray() , "未選擇");
+			System.out.println(getNum);
+			if (getNum == null)
+				break;
+			System.out.println(getNum.split(" ")[0]);
+			int change = Integer.parseInt(getNum.split(" ")[0]) - 1;
+			
+			if (typeModel.getElementAt(value).equals(venueList[change].getSportType())) {
+				System.out.println("Change Venue");
+				for (int i = 0; i < timeModel.getSize(); i++){
+					System.out.println("time:"+timeModel.getElementAt(i)+" "+timeModel.getElementAt(value)+
+							"venue:"+inputModel.getElementAt(i)+" "+venueList[change].getName());
+					/*String buff = (String)timeModel.getElementAt(i);
+					String time1 = buff.split(" ")[0];
+					buff = (String)timeModel.getElementAt(value);
+					String time2 = buff.split(" ")[0];*/
+					if (timeModel.getElementAt(i).equals(timeModel.getElementAt(value)) && 
+							inputModel.getElementAt(i).equals(venueList[change].getName())){
+						System.out.println("inner loop i:"+i);
+						String buff = (String) inputModel.getElementAt(value);
+						inputModel.set(value, inputModel.getElementAt(i));
+						inputModel.set(i, buff);
+						break;
+					}
+				}
+				break;
+			} else {
+				JOptionPane.showMessageDialog(f, "<html><font color=white>更改失敗<br>選取場地的型態不符</font><p></html>");
+			}
+		}
 	}
 }
--- src/ESS/GUI.java
+++ src/ESS/GUI.java
@@ -3,6 +3,7 @@ package ESS;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
@@ -15,10 +16,13 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
+import javax.swing.plaf.ColorUIResource;
+
 import java.awt.GridBagLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.BufferedReader;
@@ -35,6 +39,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JList;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.SystemColor;
 
 public class GUI extends JFrame {
 	static Map<Integer, String> timeMap = new HashMap<Integer, String>();
@@ -45,14 +50,9 @@ public class GUI extends JFrame {
 	JButton changeBtn = new JButton("更改賽程");
 	JButton searchBtn = new JButton("查詢賽程");
 	JButton cleanBtn = new JButton("清除");
-	JRadioButton buttonSin = new JRadioButton("sin");
-	JRadioButton buttonCos = new JRadioButton("cos");
-	JRadioButton buttonTan = new JRadioButton("tan");
-	JRadioButton buttonCsc = new JRadioButton("csc");
-	JRadioButton buttonSec = new JRadioButton("sec");
-	JRadioButton buttonCot = new JRadioButton("cot");
+	JButton addBtn = new JButton("新增");
+	JButton deleteBtn = new JButton("刪除");
 	JScrollPane scrollPane = new JScrollPane();
-	JLabel curseLab = new JLabel("箭頭(<=100)");
 	JTextField curseTF = new JTextField(10);
 	ButtonGroup controlGroup;
 	DefaultListModel venueModel = new DefaultListModel();
@@ -68,18 +68,42 @@ public class GUI extends JFrame {
 	private final JScrollPane scrollPane_1 = new JScrollPane();
 	private final JPanel scrollPanel = new JPanel();
 
+	private JButton okButton;
+
+	private JButton cancelBotton;
+
 	/**
 	 * @throws IOException
 	 * 
 	 */
 	GUI() throws IOException {
+		/*
+		okButton = new JButton("確定");
+		okButton.setForeground(Color.white);//所設置字體顏色為藍色
+		okButton.setBackground(Color.GRAY);
+		okButton.addActionListener(new ActionListener() {
+			   @Override
+			   public void actionPerformed(ActionEvent actionEvent) {
+			       JOptionPane.getRootFrame().dispose();
+			   }
+			});//對這個按鈕進行監聽
+	
+		cancelBotton = new JButton("取消");
+		cancelBotton.setBackground(Color.DARK_GRAY);
+		cancelBotton.setForeground(Color.white);
+		cancelBotton.addActionListener(new ActionListener() {
+		   @Override
+		   public void actionPerformed(ActionEvent actionEvent) {
+		       JOptionPane.getRootFrame().dispose();
+		   }
+		});//對這個按鈕進行監聽
 		
-		
-
+		JButton[] buttons = { okButton };
+		*/
 		// TODO Auto-generated method stub
 		Scanner inputScanner = new Scanner(System.in);
 		Random ran = new Random();
-		String testTeam = "台灣 韓國 俄羅斯 塞爾維亞 泰國 土耳其 義大利 美國 日本 西班牙 羅馬尼亞 墨西哥 巴西 法國 瑞士 哈薩克 奧地利 斯洛伐克 波蘭 捷克";
+		String testTeam = "台灣 韓國 俄羅斯 塞爾維亞 泰國 土耳其 義大利 美國 日本 西班牙 羅馬尼亞 墨西哥 巴西 法國 瑞士 哈薩克 奧地利 斯洛伐克 波蘭 捷克 保加利亞 匈牙利 加拿大 德國 英國 中國";
 		String testType = "棒球 羽球 籃球";
 		timeMap.put(0, "上午");
 		timeMap.put(1, "下午");
@@ -114,117 +138,152 @@ public class GUI extends JFrame {
 		controlPan.setForeground(Color.WHITE);
 		controlPan.setBackground(Color.DARK_GRAY);
 		// controlPan.setSize(600, 200);
-		controlPan.setLayout(new GridLayout(8, 1));
+		controlPan.setLayout(new GridLayout(9, 1));
 		int C = venueCounter;
 		scheduleBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// JOptionPane.showMessageDialog(dataPan, "a", "b",
-				// JFrame.EXIT_ON_CLOSE);
-				/*
-				 * Object[][] data = { { "Kelly", "Female", new Integer(16),
-				 * false, "kelly@gmail.com" }, { "Peter", "Male", new
-				 * Integer(14), false, "peter@gmail.com" }, { "Amy", "Female",
-				 * new Integer(12), false, "amy@gmail.com" }, { "Tony", "Male",
-				 * new Integer(49), true, "tony@gmail.com" } }; String[] columns
-				 * = { "Name", "Gender", "Age", "Vegetarian", "E-mail" }; list =
-				 * new JTable(data, columns);
-				 * list.setPreferredScrollableViewportSize(new Dimension(400,
-				 * 300)); dataPan.add(list); //dataPan.setVisible(true);
-				 */
+				// JOptionPane.showMessageDialog(dataPan, "a", "b", JFrame.EXIT_ON_CLOSE);
 				timeModel.clear();
 				typeModel.clear();
 				teamModel.clear();
 				venueModel.clear();
 				for (String type : typeList) {
 					for (int i = 0; i < 5; i++) {
+						// control date
 						for (int j = 0; j < 2; j++) {
-							// String type = "棒球";
+							// control time
 							Venues Venue;
 							String Team1;
 							String Team2;
 							int buff;
 							System.out.println(type);
-							System.out.println(1);
+
 							// select venue
 							do {
 								buff = ran.nextInt(C);
 							} while (!venueList[buff].getSportType().equals(type) || venueSelect[buff] == 1);
 							Venue = venueList[buff];
 							venueSelect[buff] = 1;
-							System.out.println(2);
+
 							// select team
 							do {
 								buff = ran.nextInt(teamList.length);
+								//System.out.println(1);
 							} while (teamSelect[buff] == 1);
-							System.out.print(buff + " ");
+							System.out.print(buff + " "+teamSelect[buff]+" ");
 							Team1 = teamList[buff];
 							teamSelect[buff] = 1;
 							do {
 								buff = ran.nextInt(teamList.length);
+								//System.out.println(2);
 							} while (teamSelect[buff] == 1);
-							System.out.println(buff);
+							System.out.println(buff+ " "+teamSelect[buff]);
 							Team2 = teamList[buff];
 							teamSelect[buff] = 1;
+							
+							//add data to list
 							timeModel.addElement("08/" + (19 + i) + " " + timeMap.get(j));
 							typeModel.addElement(type);
 							teamModel.addElement(Team1 + " vs. " + Team2);
 							venueModel.addElement(Venue.getName());
 
-							System.out.println("08/" + (19 + i) + " " + timeMap.get(j) + " " + Venue.getName() + " "
-									+ type + " " + Team1 + " vs. " + Team2);
+							System.out.println("08/" + (19 + i) + " " + timeMap.get(j) + " "
+									+ type + " " + Team1 + " vs. " + Team2 + " " + Venue.getName());
+							for (int k = 0; k < venueSelect.length; k++)
+								venueSelect[k] = 0;
 						}
-						for (int k = 0; k < venueSelect.length; k++)
-							venueSelect[k] = 0;
-						for (int k = 0; k < teamSelect.length; k++)
-							teamSelect[k] = 0;
-						// venueSelect = new int[venueCounter];
 					}
+					for (int k = 0; k < teamSelect.length; k++)
+						teamSelect[k] = 0;
 				}
 			}
 		});
-		scheduleBtn.setForeground(Color.WHITE);
-		scheduleBtn.setBackground(Color.DARK_GRAY);
-
+		scheduleBtn.setForeground(new Color(51, 153, 255));
+		scheduleBtn.setBackground(new Color(51, 51, 51));
+		//scheduleBtn.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		controlPan.add(scheduleBtn);
+		//Change
 		changeBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				if (venueJList.getSelectedIndex()==-1)
-					JOptionPane.showMessageDialog(dataPan, "請先選取賽程", "Message", JFrame.EXIT_ON_CLOSE);
+				if (venueJList.getSelectedIndex() == -1)
+					//JOptionPane.showOptionDialog(null, "<html><font color=white>請先選取賽程</font><p></html>", "Dialog", JOptionPane.OK_OPTION, JOptionPane.INFORMATION_MESSAGE, new ImageIcon(), buttons, buttons[0]);
+					JOptionPane.showMessageDialog(dataPan, "<html><font color=white>請先選取賽程</font><p></html>", "Message", JOptionPane.ERROR_MESSAGE);
 				else {
-					new ChangeOptionPane(timeModel,typeModel,teamModel,venueModel,venueJList.getSelectedIndex());
+					new ChangeOptionPane(timeModel, typeModel, teamModel, venueModel, venueJList.getSelectedIndex(),
+							venueList,C);
 				}
 			}
 		});
-		
-		changeBtn.setForeground(Color.WHITE);
-		changeBtn.setBackground(Color.DARK_GRAY);
+
+		changeBtn.setForeground(new Color(51, 153, 255));
+		changeBtn.setBackground(new Color(51, 51, 51));
 		controlPan.add(changeBtn);
+		//Search
 		searchBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				int mType = JOptionPane.INFORMATION_MESSAGE;
 				String resultStr = "";
-				String searchStr = JOptionPane.showInputDialog(dataPan, "請輸入查詢項目", "輸入", mType );
-				for (int i = 0;i<teamModel.getSize();i++){
-					if(searchStr=="")
+				String searchStr = JOptionPane.showInputDialog(dataPan, "<html><font color=white>請輸入查詢項目</font><p></html>", "輸入", mType);
+				for (int i = 0; i < teamModel.getSize(); i++) {
+					if (searchStr == "" || searchStr == "-1")
 						break;
-					if (((String) teamModel.getElementAt(i)).indexOf(searchStr) != -1) 
-			            resultStr += timeModel.getElementAt(i)+" "+typeModel.getElementAt(i)+" "+
-			            		teamModel.getElementAt(i)+" "+venueModel.getElementAt(i)+"\n";
-					if (((String) typeModel.getElementAt(i)).indexOf(searchStr) != -1) 
-			            resultStr += timeModel.getElementAt(i)+" "+typeModel.getElementAt(i)+" "+
-			            		teamModel.getElementAt(i)+" "+venueModel.getElementAt(i)+"\n";
-					if (((String) venueModel.getElementAt(i)).indexOf(searchStr) != -1) 
-			            resultStr += timeModel.getElementAt(i)+" "+typeModel.getElementAt(i)+" "+
-			            		teamModel.getElementAt(i)+" "+venueModel.getElementAt(i)+"\n";
+					else {
+						if (((String) teamModel.getElementAt(i)).indexOf(searchStr) != -1)
+							resultStr += timeModel.getElementAt(i) + " " + typeModel.getElementAt(i) + " "
+									+ teamModel.getElementAt(i) + " " + venueModel.getElementAt(i) + "\n";
+						if (((String) typeModel.getElementAt(i)).indexOf(searchStr) != -1)
+							resultStr += timeModel.getElementAt(i) + " " + typeModel.getElementAt(i) + " "
+									+ teamModel.getElementAt(i) + " " + venueModel.getElementAt(i) + "\n";
+						if (((String) venueModel.getElementAt(i)).indexOf(searchStr) != -1)
+							resultStr += timeModel.getElementAt(i) + " " + typeModel.getElementAt(i) + " "
+									+ teamModel.getElementAt(i) + " " + venueModel.getElementAt(i) + "\n";
+					}
 				}
 				System.out.print(resultStr);
 			}
 		});
-		searchBtn.setBackground(Color.DARK_GRAY);
-		searchBtn.setForeground(Color.WHITE);
-		
+		searchBtn.setBackground(new Color(51, 51, 51));
+		searchBtn.setForeground(new Color(51, 153, 255));
 		controlPan.add(searchBtn);
 		
+		addBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		addBtn.setForeground(new Color(51, 153, 255));
+		addBtn.setBackground(new Color(51, 51, 51));
+		controlPan.add(addBtn);
+		
+		deleteBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		deleteBtn.setForeground(new Color(51, 153, 255));
+		deleteBtn.setBackground(new Color(51, 51, 51));
+		controlPan.add(deleteBtn);
+		
+		JPanel panel = new JPanel();
+		panel.setForeground(Color.WHITE);
+		panel.setBackground(new Color(51, 51, 51));
+		controlPan.add(panel);
+		
+		JPanel panel2 = new JPanel();
+		panel2.setForeground(Color.WHITE);
+		panel2.setBackground(new Color(51, 51, 51));
+		controlPan.add(panel2);
+		
+		JPanel panel3 = new JPanel();
+		panel3.setForeground(Color.WHITE);
+		panel3.setBackground(new Color(51, 51, 51));
+		controlPan.add(panel3);
+		
+		JPanel panel4 = new JPanel();
+		panel4.setForeground(Color.WHITE);
+		panel4.setBackground(Color.DARK_GRAY);
+		//controlPan.add(panel4);
+
 		cleanBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				timeModel.clear();
@@ -234,7 +293,7 @@ public class GUI extends JFrame {
 			}
 		});
 		cleanBtn.setForeground(Color.WHITE);
-		cleanBtn.setBackground(Color.DARK_GRAY);
+		cleanBtn.setBackground(new Color(51, 51, 51));
 
 		controlPan.add(cleanBtn);
 		/*
@@ -251,7 +310,7 @@ public class GUI extends JFrame {
 		 */
 
 		// dataPan.add(list);
-
+		//data panel
 		dataPan.setLayout(new GridLayout(1, 4));
 		getContentPane().add(controlPan, BorderLayout.WEST);
 		dataPan.setBackground(Color.DARK_GRAY);
@@ -265,7 +324,7 @@ public class GUI extends JFrame {
 		timeJList.addMouseListener(new sync());
 		venueJList.addMouseListener(new sync());
 		typeJList.addMouseListener(new sync());
-		
+
 		scrollPane_1.setViewportView(scrollPanel);
 		scrollPanel.setLayout(new GridLayout(1, 4));
 		timeJList.setForeground(Color.WHITE);
@@ -280,19 +339,22 @@ public class GUI extends JFrame {
 		venueJList.setForeground(Color.WHITE);
 		venueJList.setBackground(Color.DARK_GRAY);
 		scrollPanel.add(venueJList);
-		
-		
+
 	}
 
 	public static void main(String[] args) throws IOException {
-		setUIFont (new javax.swing.plaf.FontUIResource("Microsoft JhengHei UI",1,16));
+		UIManager.put("OptionPane.background",new Color(51, 51, 51));
+		UIManager.put("Panel.background",new Color(51, 51, 51));
+		UIManager.put("OptionPane.okButtonbackground",Color.BLUE);
+		UIManager.put("Panel.Text",Color.WHITE);
+		setUIFont(new javax.swing.plaf.FontUIResource("Microsoft JhengHei UI", 4, 18));
 		GUI s = new GUI();
 		s.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		s.setSize(1024, 768);
 		s.setTitle("ESS");
 		s.setVisible(true);
 	}
-	
+
 	public static void setUIFont(javax.swing.plaf.FontUIResource f) {
 		java.util.Enumeration keys = UIManager.getDefaults().keys();
 		while (keys.hasMoreElements()) {
@@ -306,7 +368,7 @@ public class GUI extends JFrame {
 	class sync implements MouseListener {
 		public void mouseClicked(MouseEvent evt) {
 			JList list = (JList) evt.getSource();
-			switch(evt.getClickCount()){
+			switch (evt.getClickCount()) {
 			case 1:
 				int index = list.locationToIndex(evt.getPoint());
 				venueJList.setSelectedIndex(index);
@@ -320,16 +382,16 @@ public class GUI extends JFrame {
 			case 3:
 				JOptionPane.showMessageDialog(dataPan, "World", "Message", JFrame.EXIT_ON_CLOSE);
 			}
-			/*else if (evt.getClickCount() == 2) {
-
-				// Double-click detected
-				JOptionPane.showMessageDialog(dataPan, "Hello", "Message", JFrame.EXIT_ON_CLOSE);
-			} 
-			else if (evt.getClickCount() == 3) {
-
-				// Triple-click detected
-				int index = list.locationToIndex(evt.getPoint());
-			}*/
+			/*
+			 * else if (evt.getClickCount() == 2) {
+			 * 
+			 * // Double-click detected JOptionPane.showMessageDialog(dataPan,
+			 * "Hello", "Message", JFrame.EXIT_ON_CLOSE); } else if
+			 * (evt.getClickCount() == 3) {
+			 * 
+			 * // Triple-click detected int index =
+			 * list.locationToIndex(evt.getPoint()); }
+			 */
 		}
 
 		@Override
